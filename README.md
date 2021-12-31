5236f8cfc4a9b1ccb1aab04ee3139ba77167e4d4
Greg Fiumara <greg@gregfiumara.com>
2021-12-31 16:07:53 -0500

    Initial commit.
    Adds dark mode, open/close information, more logos, and other tweaks
    since the hand-generated site.

---
+++ README.md
@@ -0,0 +1,11 @@
+# moco.beer
+
+Scripts and data powering [https://moco.beer/](https://moco.beer). The site is
+generated statically since the information does not change often.
+
+## Add a Brewery
+
+Follow the pattern in [mocobeer.json](blob/master/mocobeer.json) to
+[submit a pull request](https://github.com/gfiumara/mocobeer/pulls), or
+[submit an issue](https://github.com/gfiumara/mocobeer/issues) with the
+relevant brewery information.
---
+++ generate_mocobeer_static.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=ascii :
+
+import argparse
+from datetime import datetime
+import json
+import os
+import sys
+from types import SimpleNamespace
+
+header = '''<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<style>
+			:root { --body-bg:#FFFFFF; --body-color:#000000; --footer-color:lightgray; }
+			@media(prefers-color-scheme: dark) {
+				:root { --body-bg:#000000; --body-color:#FFFFFF; --footer-color:lightgray; }
+				.invert { filter:invert(1); }
+			}
+
+			body { padding:15px; background: var(--body-bg); color: var(--body-color); }
+			h1, h2 { text-align:center; }
+			.center { text-align:center; }
+			.store::before { content:"\\1F6D2 "; }
+			.map::before { content:"\\1F4CD "; }
+			.phone::before { content:"\\260E\\200D\\FE0F "; }
+			.website::before { content:"\\1F5A5 "; }
+			.social img { height: 2ex; }
+			.closed { color:red; }
+			.separator { color:var(--footer-color); }
+			footer { color:var(--footer-color); text-align:center; font-size:8pt; }
+			footer a { color:var(--footer-color); }
+			section { padding-top: 2ex; }
+
+			.invert imge
+		</style>
+		<title>Beer in Montgomery County, Maryland</title>
+	</head>
+	<body>
+		<h1>Beer in Montgomery County, Maryland</h1>
+		<div class=\"center\"><a href=\"https://thinkmoco.com/made-in-moco/\"><img src=\"drinklocal_mocomade.png\" srcset=\"logos/drinklocal_mocomade.png 1x, logos/drinklocal_mocomade@2x.png 2x\" alt=\"Drink Local. Moco Made.\" title=\"#mocomade\"></a></div>
+'''
+
+google_map = '''		<section>
+			<h2>Notable Restaurants, Bottle Shops, and Bars</h2>
+			<iframe class="d-block mx-auto" src="https://www.google.com/maps/d/u/1/embed?mid=1HcsTMRMiEsDwHCUf9J7_T5jXwnAVGaIZ&z=10&ll=39.14789573526428,-77.2005358102505" width="640" height="480"></iframe>
+		</section>
+'''
+
+footer = f'''		<footer>
+			<hr />
+			Made and <a href="https://github.com/gfiumara/mocobeer">open sourced</a> in Gaithersburg by <a href="https://gregfiumara.com">Greg Fiumara</a> and <a href="https://github.com/gfiumara/mocobeer/contributors">contributors</a>.<br />
+			Last updated on {datetime.now().strftime("%d %B %Y at %I:%M:%S %p")}.<br />
+			&copy; 2018&ndash;2022 <a href="https://gregfiumara.com">Greg Fiumara</a>. <a href="LICENSE">License</a>.
+		</footer>
+	</body>
+</html>
+'''
+
+def parse_arguments():
+	parser = argparse.ArgumentParser(
+	    description = "Generate a static HTML page for moco.beer")
+	parser.add_argument('-i', '--input',
+	    required = True,
+	    metavar = "input.json",
+	    dest = "input_file",
+	    help = "Input JSON file with location information")
+	parser.add_argument('-o', '--output',
+	    required = True,
+	    dest = "output_file",
+	    metavar = "output.html",
+	    help = "Output HTML file generated from input JSON")
+	parser.add_argument('-f', '--force',
+	    dest = "force",
+	    action = "store_true")
+
+	args = parser.parse_args()
+	if not os.path.exists(args.input_file):
+		print(f"Input JSON \"{args.input_file}\" does not exist")
+		sys.exit(1)
+	if os.path.exists(args.output_file) and not args.force:
+		answer = input(f"Output file \"{args.output_file}\" exists. "
+		    "Overwrite? [y/n]: ")
+		if answer.lower() != 'y' and answer.lower() != "yes":
+			sys.exit(0)
+
+	return args
+
+def check_required_location_keys(location):
+	required_keys = ["name", "slug", "types"]
+	for key in required_keys:
+		if location.get(key) is None:
+			raise KeyError(f'Property "{key}" not found ({location})')
+
+def separator(counter):
+	if counter == 0:
+		return ""
+
+	return "<span class=\"separator\"> | </span>"
+
+def phone_format(n):
+	str_n = str(n)
+	return f"({str_n[0:3]}) {str_n[3:6]}-{str_n[6:]}"
+
+def contains_keys(location, key_list):
+	for key in key_list:
+		if location.get(key) is not None:
+			return True
+	return False
+
+def html_for_location(location):
+	i = 0
+	base_tab = '\t\t\t'
+	key = lambda k : location.get(k)
+	add_field = lambda counter, existing_html, new_field : (counter + 1, existing_html + separator(counter) + new_field)
+	begin_section = lambda counter, existing_html : (0, existing_html + f'\n{base_tab}\t\t<li>')
+	end_section = lambda counter, existing_html : (counter, existing_html + '</li>\n')
+
+	location_keys = ["address", "phone_number"]
+	social_keys = ["twitter_handle", "facebook_url", "instagram_handle", "yelp_url", "trip_advisor_url"]
+	beer_keys = ["untappd_url", "beer_advocate_url", "rate_beer_url", "brewery_db_url"]
+
+	s = f'{base_tab}<dt id="{key("slug")}">'
+	if key("open_status") is not None and key("open_status").lower() != "open":
+		s += f' <span class="closed">{key("open_status")}: </span>'
+	s += f'{key("name")}</dt>\n{base_tab}<dd>\n{base_tab}\t<ul>'
+
+	# Address and phone number
+	if contains_keys(location, location_keys):
+		i, s = begin_section(i, s)
+		if key("address") is not None and key("google_maps_url") is not None:
+			i, s = add_field(i, s, f'<a href="{key("google_maps_url")}" class="map">{key("address")}</a>')
+		if key("phone_number") is not None:
+			i, s = add_field(i, s,  f'<a href="tel://{key("phone_number")}" class="phone">{phone_format(key("phone_number"))}</a>')
+		i, s = end_section(i, s)
+
+	# Social
+	if contains_keys(location, social_keys):
+		i, s = begin_section(i, s)
+		if key("website") is not None:
+			i, s = add_field(i, s, f'<a href="{key("website")}" class="social website">Website</a>')
+		if key("twitter_handle") is not None:
+			i, s = add_field(i, s, f'<a href="https://twitter.com/{key("twitter_handle")}" class="social twitter"><img src="logos/twitter.svg" alt="Twitter Logo" /> Twitter</a>')
+		if key("facebook_url") is not None:
+			i, s = add_field(i, s, f'<a href="{key("facebook_url")}" class="social facebook"><img src="logos/facebook.svg" alt="Facebook Logo" /> Facebook</a>')
+		if key("facebook_url") is not None:
+			i, s = add_field(i, s, f'<a href="https://instagram.com/{key("instagram_handle")}" class="social instagram"><img src="logos/instagram.svg" alt="Instagram Logo" class="invert" /> Instagram</a>')
+		if key("yelp_url") is not None:
+			i, s = add_field(i, s, f'<a href="{key("yelp_url")}" class="social yelp"><img src="logos/yelp.svg" alt="Yelp Logo" /> Yelp</a>')
+		if key("trip_advisor_url") is not None:
+			i, s = add_field(i, s, f'<a href="{key("trip_advisor_url")}" class="social trip_advisor"><img src="logos/trip_advisor.svg" alt="Trip Advisor Logo" class="invert" /> Trip Advisor</a>')
+		i, s = end_section(i, s)
+
+	# Beer info
+	if contains_keys(location, beer_keys):
+		i, s = begin_section(i, s)
+		if key("untappd_url") is not None:
+			i, s = add_field(i, s, f'<a href="{key("untappd_url")}" class="social untappd"><img src="logos/untappd.svg" alt="Untappd Logo" /> Untappd</a>')
+		if key("beer_advocate_url") is not None:
+			i, s = add_field(i, s, f'<a href="{key("beer_advocate_url")}" class="social beer_advocate"><img src="beer_advocate.png" srcset="logos/beer_advocate.png 1x, logos/beer_advocate@2x.png 2x" alt="Beer Advocate Logo" /> Beer Advocate</a>')
+		if key("ratebeer_url") is not None:
+			i, s = add_field(i, s, f'<a href="{key("ratebeer_url")}" class="social ratebeer"><img src="logos/ratebeer.svg" alt="RateBeer Logo" /> RateBeer</a>')
+		if key("brewerydb_url") is not None:
+			i, s = add_field(i, s, f'<a href="{key("brewerydb_url")}" class="social brewerydb"><img src="brewerydb.png" srcset="logos/brewerydb.png 1x, logos/brewerydb@2x.png 2x" alt="BreweryDB Logo" /> BreweryDB</a>')
+		i, s = end_section(i, s)
+
+	# Online store
+	if key("store_url") is not None:
+		i, s = begin_section(i, s)
+		i, s = add_field(i, s, f'<a href="{key("store_url")}" class="store">Online Store</a>')
+		i, s = end_section(i, s)
+
+	if key("notes") is not None and len(key("notes")) > 0:
+		s += f"{base_tab}\t<li><strong>Notes:</strong>\n"
+		s += f"{base_tab}\t\t<ul>\n"
+		for note in key("notes"):
+			s += f"{base_tab}\t\t\t<li>{note}</li>\n"
+		s += f"{base_tab}\t\t</ul></li>\n"
+
+	s += f"{base_tab}\t</ul>\n{base_tab}</dd>\n"
+	return s
+
+################################################################################
+
+if __name__ == '__main__':
+	args = parse_arguments()
+
+	html = header
+	begin_section = lambda slug, caption : f"\t<section>\n\t\t<h2 id=\"{slug}\">{caption}</h2>\n\t\t<dl>\n"
+	end_section = "\t\t</dl>\n\t</section>\n"
+
+	with open(args.input_file) as file:
+		locations = json.load(file)
+
+	# Craft breweries
+	html += begin_section("craft", "Independent Craft Breweries")
+	for location in locations:
+		check_required_location_keys(location)
+		if not "craft_brewery" in location["types"]:
+			continue
+		html += html_for_location(location)
+	html += end_section
+
+	html += google_map
+	html += footer
+
+	with open(args.output_file, 'w') as file:
+		file.write(html)
