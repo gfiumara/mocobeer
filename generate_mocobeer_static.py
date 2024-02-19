42d867117417ef403e576db871218e6f9329dc5b
Greg Fiumara <greg@gregfiumara.com>
2024-02-18 19:51:34 -0500

    Update Bootstrap to 5.3.2.
    We don't really use any styles, so just inherit the default typography

--- generate_mocobeer_static.py
+++ generate_mocobeer_static.py
@@ -13,7 +13,7 @@ header = '''<!doctype html>
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 		<style>
 			:root { --body-bg:#FFFFFF; --body-color:#000000; --footer-color:lightgray; }
 			@media(prefers-color-scheme: dark) {
