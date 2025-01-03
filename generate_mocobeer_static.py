28741ee5e8d33dade392574cad32b29d3390bd8c
Greg Fiumara <greg@gregfiumara.com>
2025-01-02 21:15:11 -0500

    Update Bootstrap CSS

--- generate_mocobeer_static.py
+++ generate_mocobeer_static.py
@@ -13,7 +13,7 @@ header = '''<!doctype html>
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 		<style>
 			:root { --body-bg:#FFFFFF; --body-color:#000000; --footer-color:lightgray; }
 			@media(prefers-color-scheme: dark) {
